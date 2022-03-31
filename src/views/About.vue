ec477d69b7f255e335ab7f416f5b6fd47ff48bf4
Peter Voloșenco <petervolosenco@gmail.com>
2022-03-31 20:47:15 +0100

    wave overlay

--- src/views/About.vue
+++ src/views/About.vue
@@ -2,29 +2,35 @@
   <div class="about">
     <h1>This is an about page</h1>
 
-    <div class="parallax-overlay">
+    <div class="parallax-overlay" :style="{ backgroundPosition: position, backgroundSize: sizeAlter }">
       <img src="@/img/ice-bg.jpg"/>
     </div>
-    <div class="parallax-overlay">
+
+    <div class="parallax-overlay" :style="{ backgroundPosition: position, backgroundSize: size }">
       <img src="@/img/ice-bg2.jpg"/>
     </div>
-    <div class="parallax-overlay">
+
+    <div class="parallax-overlay" :style="{ backgroundPosition: position, backgroundSize: sizeAlter }">
       <img src="@/img/ice-bg3.jpg"/>
     </div>
-      <div class="parallax-overlay">
+
+    <div class="parallax-overlay" :style="{ backgroundPosition: position, backgroundSize: size }">
       <img src="@/img/ice-bg4.jpg"/>
     </div>
+
   </div>
 </template>
 
 <style scoped>
   img {width:100%; opacity: 0.6;}
+
   .parallax-overlay {
     background-image: url("~@/img/Festive-Pattern-blue.png");
     background-attachment: fixed;
     background-size: cover;
     background-position: center;
-    background-repeat: repeat-x;
+    background-repeat: repeat;
+    margin: 100px 0;
   }
   .parallax-overlay:nth-child(2n) {
     background-position: -100%;
@@ -38,6 +44,7 @@
     data() {
       return {
         size: "auto",
+        sizeAlter: "auto",
         attachment: "fixed",
         position: "center",
       }
@@ -50,13 +57,15 @@
         console.log(window.scrollY);
         let scrollAmount = window.scrollY;
         let offset = 0;
-        var scroll = scrollAmount * 0.1;
+        var scrollPozition = scrollAmount * 0.1;
+        var scrollSize = scrollAmount * 0.001;
 
-        if (scroll <= offset) {
-          self.size = "auto";
-          self.position = "right " + (offset - scroll) + "px";
-        }
-           if (window.innerWidth > 1024) {
+        if (window.innerWidth > 1024) {
+          if (scrollAmount >= offset) {
+            self.size = (100 + (20 * scrollSize))  + "%";
+            self.sizeAlter = (100 - (20 * scrollSize))  + "%";
+            self.position = "center " + (scrollPozition) + "px";
+          }
         } else {
           self.size = "auto";
           self.position = "right center";
